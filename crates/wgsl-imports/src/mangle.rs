6301bd8c5ab26c6253697c2e20e3c32ff91a29a8
Mathis Brossier <mathis.brossier@gmail.com>
2024-09-03 20:57:21 +0200

    first step mangling with hash mangler

--- crates/wgsl-imports/src/mangle.rs
+++ crates/wgsl-imports/src/mangle.rs
@@ -6,6 +6,7 @@ use itertools::chain;
 use wgsl_parse::syntax::*;
 use wgsl_parse::visit_fields;
 use wgsl_parse::visit_variants;
+use wgsl_parse_macros::query;
 
 use crate::resolve::FileResource;
 use crate::resolve::{FileResolver, ImportError, Module, Resolver};
@@ -27,117 +28,163 @@ impl Mangler<FileResolver> for FileManglerHash {
     }
 }
 
+fn mod_visit_exprs(module: &mut TranslationUnit) -> impl Iterator<Item = &mut Expression> {
+    module
+        .visit_mut()
+        .global_declarations()
+        .each()
+        .flat_map(visit_variants! {
+            GlobalDeclaration::Declaration(x) => x.visit_mut().initializer().some(),
+            GlobalDeclaration::Function(x) => visit_fields!(x, {
+                body => body.visit_mut().statements().each().flat_map(stat_visit_exprs),
+            }),
+        })
+}
+
+fn mod_visit_type_exprs(module: &mut TranslationUnit) -> impl Iterator<Item = &mut TypeExpression> {
+    module
+        .visit_mut()
+        .global_declarations()
+        .each()
+        .flat_map(visit_variants! {
+            GlobalDeclaration::Declaration(x) => x.typ.visit_mut().some(),
+            GlobalDeclaration::TypeAlias(x) => x.typ.visit_mut(),
+            GlobalDeclaration::Struct(x) => x.members.visit_mut().each().typ(),
+            GlobalDeclaration::Function(x) => visit_fields!(x, {
+                parameters => parameters.visit_mut().each().typ(),
+                return_type => return_type.visit_mut().some(),
+            }),
+        })
+}
+
+fn expr_visit_exprs(expr: &mut Expression) -> impl Iterator<Item = &mut Expression> {
+    visit_variants!(expr, {
+        Expression::Parenthesized(x) => x.visit_mut(),
+        Expression::NamedComponent(x) => expr_visit_exprs(&mut x.base),
+        Expression::Indexing(x) => visit_fields!(x, {
+            base => expr_visit_exprs(base),
+            index => expr_visit_exprs(index),
+        }),
+        Expression::Unary(x) => expr_visit_exprs(&mut x.operand),
+        Expression::Binary(x) => visit_fields!(x, {
+            left => expr_visit_exprs(left),
+            right => expr_visit_exprs(right),
+        }),
+        Expression::FunctionCall(x) => x.arguments.visit_mut().each().flat_map(expr_visit_exprs),
+    })
+}
+
+fn stat_visit_exprs(stat: &mut Statement) -> impl Iterator<Item = &mut Expression> {
+    visit_variants!(stat, {
+        Statement::Compound(x) => x.statements.visit_mut().each().flat_map(stat_visit_exprs),
+        Statement::Assignment(x) => visit_fields!(x, {
+            lhs => lhs.visit_mut(),
+            rhs => rhs.visit_mut(),
+        }),
+        Statement::Increment(x) => x.visit_mut(),
+        Statement::Decrement(x) => x.visit_mut(),
+        Statement::If(x) => visit_fields!(x, {
+            if_clause => {
+                let (expr, stat) = if_clause;
+                chain!(expr.visit_mut(), stat.statements.visit_mut().each().flat_map(stat_visit_exprs))
+            },
+            else_if_clauses => else_if_clauses.visit_mut().each().flat_map(|(expr, stat)| {
+                chain!(expr.visit_mut(), stat.statements.visit_mut().each().flat_map(stat_visit_exprs))
+            }),
+            else_clause => else_clause.visit_mut().some().statements().each().flat_map(stat_visit_exprs),
+        }),
+        Statement::Switch(x) => visit_fields!(x , {
+            expression => expression.visit_mut(),
+            clauses => clauses.visit_mut().each().flat_map(visit_fields! {
+                case_selectors => case_selectors.visit_mut().each().match_expression(),
+                body => body.statements.visit_mut().each().flat_map(stat_visit_exprs),
+            }),
+        }),
+        Statement::Loop(x) => visit_fields!(x, {
+            body => body.statements.visit_mut().each().flat_map(stat_visit_exprs),
+            continuing => continuing.visit_mut().some().flat_map(visit_fields! {
+                body => body.statements.visit_mut().each().flat_map(stat_visit_exprs),
+                break_if => break_if.visit_mut().some(),
+            }),
+        }),
+        Statement::For(x) => visit_fields!(x, {
+            initializer => initializer.visit_mut().some().flat_map(|x| stat_visit_exprs(x)),
+            condition => condition.visit_mut().some(),
+            update => update.visit_mut().some().flat_map(|x| stat_visit_exprs(x)),
+            body => body.statements.visit_mut().each().flat_map(stat_visit_exprs),
+        }),
+        Statement::While(x) => visit_fields!(x, {
+            condition => condition.visit_mut(),
+            body => body.statements.visit_mut().each().flat_map(stat_visit_exprs),
+        }),
+        Statement::Return(x) => x.visit_mut().some(),
+        Statement::FunctionCall(x) => x.arguments.visit_mut().each(),
+        Statement::ConstAssert(x) => x.expression.visit_mut(),
+        Statement::Declaration(x) => x.visit_mut().initializer().some(),
+    })
+}
+
+fn replace_imported_ident(module: &mut TranslationUnit, old_ident: &str, new_ident: &str) {
+    for type_expr in mod_visit_type_exprs(module) {
+        if type_expr.name == old_ident {
+            type_expr.name = new_ident.to_string();
+        }
+    }
+
+    fn expr_replace(expr: &mut Expression, old_ident: &str, new_ident: &str) {
+        match expr {
+            Expression::Parenthesized(expr) => {
+                expr_visit_exprs(expr).for_each(|x| expr_replace(x, old_ident, new_ident))
+            }
+            Expression::NamedComponent(x) => {
+                expr_visit_exprs(&mut x.base).for_each(|x| expr_replace(x, old_ident, new_ident))
+            }
+            Expression::Indexing(x) => {
+                expr_visit_exprs(&mut x.base).for_each(|x| expr_replace(x, old_ident, new_ident));
+                expr_visit_exprs(&mut x.index).for_each(|x| expr_replace(x, old_ident, new_ident));
+            }
+            Expression::Unary(x) => {
+                expr_visit_exprs(&mut x.operand).for_each(|x| expr_replace(x, old_ident, new_ident))
+            }
+            Expression::Binary(x) => {
+                expr_visit_exprs(&mut x.left).for_each(|x| expr_replace(x, old_ident, new_ident));
+                expr_visit_exprs(&mut x.right).for_each(|x| expr_replace(x, old_ident, new_ident));
+            }
+            Expression::FunctionCall(call_expr) => {
+                if call_expr.name == old_ident {
+                    call_expr.name = new_ident.to_string();
+                }
+                call_expr
+                    .arguments
+                    .visit_mut()
+                    .each()
+                    .flat_map(expr_visit_exprs)
+                    .for_each(|x| expr_replace(x, old_ident, new_ident));
+            }
+            Expression::Type(type_expr) => {
+                if type_expr.name == old_ident {
+                    type_expr.name = new_ident.to_string();
+                }
+            }
+            _ => (),
+        }
+    }
+
+    for expr in mod_visit_exprs(module) {
+        expr_replace(expr, old_ident, new_ident)
+    }
+}
+
 impl<R: Resolver> Module<R> {
     pub fn mangle(&mut self, mangler: &impl Mangler<R>) -> Result<(), ImportError> {
-        for (resource, items) in self.imports.clone() {
+        for (resource, items) in &self.imports {
             for item in items {
                 let old_ident = item.rename.as_ref().unwrap_or(&item.name);
                 let new_ident = mangler.mangle(&resource, &item.name);
-                self.replace(&old_ident, &new_ident);
+                replace_imported_ident(&mut self.source, &old_ident, &new_ident);
             }
         }
 
         Ok(())
-    }
-
-    fn visit_type_exprs(&mut self) -> impl Iterator<Item = &mut TypeExpression> {
-        fn expr_type_exprs(x: &mut Expression) -> impl Iterator<Item = &mut TypeExpression> {
-            visit_variants!(x, {
-                Expression::Parenthesized(x) => expr_type_exprs(x),
-                Expression::NamedComponent(x) => expr_type_exprs(&mut x.base),
-                Expression::Indexing(x) => visit_fields!(x, {
-                    base => expr_type_exprs(base),
-                    index => expr_type_exprs(index),
-                }),
-                Expression::Unary(x) => expr_type_exprs(&mut x.operand),
-                Expression::Binary(x) => visit_fields!(x, {
-                    left => expr_type_exprs(left),
-                    right => expr_type_exprs(right),
-                }),
-                Expression::FunctionCall(x) => x.arguments.visit_mut().each().flat_map(expr_type_exprs),
-                Expression::Type(x) => x.visit_mut(),
-            })
-        }
-
-        fn stat_type_exprs(x: &mut Statement) -> impl Iterator<Item = &mut TypeExpression> {
-            visit_variants!(x, {
-                Statement::Compound(x) => x.statements.visit_mut().each().flat_map(stat_type_exprs),
-                Statement::Assignment(x) => visit_fields!(x, {
-                    lhs => expr_type_exprs(lhs),
-                    rhs => expr_type_exprs(rhs),
-                }),
-                Statement::Increment(x) => expr_type_exprs(x),
-                Statement::Decrement(x) => expr_type_exprs(x),
-                Statement::If(x) => visit_fields!(x, {
-                    if_clause => {
-                        let (expr, stat) = if_clause;
-                        chain!(expr_type_exprs(expr), stat.statements.visit_mut().each().flat_map(stat_type_exprs))
-                    },
-                    else_if_clauses => else_if_clauses.visit_mut().each().flat_map(|(expr, stat)| {
-                        chain!(expr_type_exprs(expr), stat.statements.visit_mut().each().flat_map(stat_type_exprs))
-                    }),
-                    else_clause => else_clause.visit_mut().some().statements().each().flat_map(stat_type_exprs),
-                }),
-                Statement::Switch(x) => visit_fields!(x , {
-                    expression => expr_type_exprs(expression),
-                    clauses => clauses.visit_mut().each().flat_map(visit_fields! {
-                        case_selectors => case_selectors.visit_mut().each().match_expression().flat_map(expr_type_exprs),
-                        body => body.statements.visit_mut().each().flat_map(stat_type_exprs),
-                    }),
-                }),
-                Statement::Loop(x) => visit_fields!(x, {
-                    body => body.statements.visit_mut().each().flat_map(stat_type_exprs),
-                    continuing => continuing.visit_mut().some().flat_map(visit_fields! {
-                        body => body.statements.visit_mut().each().flat_map(stat_type_exprs),
-                        break_if => break_if.visit_mut().some().flat_map(expr_type_exprs),
-                    }),
-                }),
-                Statement::For(x) => visit_fields!(x, {
-                    initializer => initializer.visit_mut().some().flat_map(|x| stat_type_exprs(x)),
-                    condition => condition.visit_mut().some().flat_map(expr_type_exprs),
-                    update => update.visit_mut().some().flat_map(|x| stat_type_exprs(x)),
-                    body => body.statements.visit_mut().each().flat_map(stat_type_exprs),
-                }),
-                Statement::While(x) => visit_fields!(x, {
-                    condition => expr_type_exprs(condition),
-                    body => body.statements.visit_mut().each().flat_map(stat_type_exprs),
-                }),
-                Statement::Return(x) => x.visit_mut().some().flat_map(expr_type_exprs),
-                Statement::FunctionCall(x) => x.arguments.visit_mut().each().flat_map(expr_type_exprs),
-                Statement::ConstAssert(x) => expr_type_exprs(&mut x.expression),
-                Statement::Declaration(x) => visit_fields!(x, {
-                    typ => typ.visit_mut().some(),
-                    initializer => initializer.visit_mut().some().flat_map(expr_type_exprs),
-                }),
-            })
-        }
-
-        let type_exprs = self
-            .source
-            .visit_mut()
-            .global_declarations()
-            .each()
-            .flat_map(visit_variants! {
-                GlobalDeclaration::Declaration(x) => visit_fields!(x, {
-                    typ => typ.visit_mut().some(),
-                    initializer => initializer.visit_mut().some().flat_map(expr_type_exprs),
-                }),
-                GlobalDeclaration::TypeAlias(x) => x.typ.visit_mut(),
-                GlobalDeclaration::Struct(x) => x.members.visit_mut().each().typ(),
-                GlobalDeclaration::Function(x) => visit_fields!(x, {
-                    parameters => parameters.visit_mut().each().typ(),
-                    return_type => return_type.visit_mut().some(),
-                    body => body.visit_mut().statements().each().flat_map(stat_type_exprs),
-                }),
-            });
-        type_exprs
-    }
-
-    fn replace(&mut self, old_ident: &str, new_ident: &str) {
-        for type_expr in self.visit_type_exprs() {
-            if type_expr.name == old_ident {
-                type_expr.name = new_ident.to_string();
-            }
-        }
     }
 }
