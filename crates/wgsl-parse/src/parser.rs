4e744920cba271dc62bb2754f63acbbaea863a42
Mathis Brossier <mathis.brossier@gmail.com>
2024-09-21 18:05:53 +0200

    allow parsing individually some of the syntax nodes

--- crates/wgsl-parse/src/parser.rs
+++ crates/wgsl-parse/src/parser.rs
@@ -11,9 +11,11 @@ lalrpop_mod!(
     wgsl_recognize
 );
 
+use std::str::FromStr;
+
 use lalrpop_util::lalrpop_mod;
 
-use crate::{error::SpannedError, lexer::Lexer, syntax};
+use crate::{error::SpannedError, lexer::Lexer, syntax, Error};
 
 pub struct Parser;
 
@@ -30,6 +32,58 @@ impl Parser {
         let parser = wgsl::TranslationUnitParser::new();
         let res = parser.parse(&mut lexer);
         res.map_err(|e| SpannedError::new(e, lexer.source()))
+    }
+}
+
+impl FromStr for syntax::TranslationUnit {
+    type Err = Error;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let lexer = Lexer::new(source);
+        let parser = wgsl::TranslationUnitParser::new();
+        let res = parser.parse(lexer);
+        res.map_err(|e| SpannedError::new(e, source).into_owned())
+    }
+}
+impl FromStr for syntax::GlobalDirective {
+    type Err = Error;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let lexer = Lexer::new(source);
+        let parser = wgsl::GlobalDirectiveParser::new();
+        let res = parser.parse(lexer);
+        res.map_err(|e| SpannedError::new(e, source).into_owned())
+    }
+}
+impl FromStr for syntax::GlobalDeclaration {
+    type Err = Error;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let lexer = Lexer::new(source);
+        let parser = wgsl::GlobalDeclParser::new();
+        let res = parser.parse(lexer);
+        res.map_err(|e| SpannedError::new(e, source).into_owned())
+    }
+}
+impl FromStr for syntax::Expression {
+    type Err = Error;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let lexer = Lexer::new(source);
+        let parser = wgsl::ExpressionParser::new();
+        let res = parser.parse(lexer);
+        res.map_err(|e| SpannedError::new(e, source).into_owned())
+    }
+}
+#[cfg(feature = "imports")]
+impl FromStr for syntax::Import {
+    type Err = Error;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let lexer = Lexer::new(source);
+        let parser = wgsl::ImportParser::new();
+        let res = parser.parse(lexer);
+        res.map_err(|e| SpannedError::new(e, source).into_owned())
     }
 }
 
